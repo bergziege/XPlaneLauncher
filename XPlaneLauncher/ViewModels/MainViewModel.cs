e54dc1ea4db21afbe1f04f675c1ef4742fca1d3a
Bernd Boruttau <berndnet2000@web.de>
2019-10-04 09:23:49 +0200

    fixes - Edit Mode on selection change. Edit mode now ends when aircraft selection changes

--- XPlaneLauncher/ViewModels/MainViewModel.cs
+++ XPlaneLauncher/ViewModels/MainViewModel.cs
@@ -55,14 +55,17 @@ namespace XPlaneLauncher.ViewModels
             MapAircraftItems.Clear();
             foreach (AircraftDto aircraftDto in aircraftDtos)
             {
-                IAircraftItemViewModel itemViewModel = new AircraftItemViewModel(_aircraftService).Initialize(aircraftDto);
+                IAircraftItemViewModel itemViewModel =
+                    new AircraftItemViewModel(_aircraftService).Initialize(aircraftDto);
                 PropertyChangedEventManager.AddListener(itemViewModel, this, nameof(itemViewModel.IsSelected));
-                PropertyChangedEventManager.AddListener(itemViewModel, this, nameof(itemViewModel.IsInTargetSelectionMode));
+                PropertyChangedEventManager.AddListener(itemViewModel, this,
+                    nameof(itemViewModel.IsInTargetSelectionMode));
                 PropertyChangedEventManager.AddListener(itemViewModel, this, nameof(itemViewModel.PlannedRoutePoints));
                 Aircrafts.Add(itemViewModel);
                 MapAircraftItems.Add(new AircraftItem(aircraftDto.LiveryName, aircraftDto.Lat, aircraftDto.Lon,
                     itemViewModel));
             }
+
             UpdateTargetPaths();
             UpdatePathsPoints();
         }
@@ -106,10 +109,11 @@ namespace XPlaneLauncher.ViewModels
             get => _selectedAircraft;
             set
             {
-                if (IsMapInSelectionMode)
+                if (_selectedAircraft != value && _selectedAircraft != null && _selectedAircraft.EndTargetSelectionModeCommand.CanExecute())
                 {
-                    return;
+                    _selectedAircraft.EndTargetSelectionModeCommand.Execute();
                 }
+
                 _changingSelectedAicraftFromList = true;
                 if (_selectedAircraft != null)
                 {
@@ -120,8 +124,9 @@ namespace XPlaneLauncher.ViewModels
                 StartSimCommand.RaiseCanExecuteChanged();
                 if (value != null)
                 {
-                    value.IsSelected = true; 
+                    value.IsSelected = true;
                 }
+
                 if (!_changingSelectedAicraftFromMap && SelectedAircraft != null)
                 {
                     MapCenter = new Location(SelectedAircraft.AircraftDto.Lat, SelectedAircraft.AircraftDto.Lon);
@@ -151,7 +156,8 @@ namespace XPlaneLauncher.ViewModels
             {
                 if (_applyTargetCommand == null)
                 {
-                    _applyTargetCommand = new DelegateCommand<Location>(ApplyMapCenterAsTargetOnAircraftInSelectionMode);
+                    _applyTargetCommand =
+                        new DelegateCommand<Location>(ApplyMapCenterAsTargetOnAircraftInSelectionMode);
                 }
 
                 return _applyTargetCommand;
@@ -163,7 +169,7 @@ namespace XPlaneLauncher.ViewModels
 
         private void ApplyMapCenterAsTargetOnAircraftInSelectionMode(Location target)
         {
-            foreach (IAircraftItemViewModel aircraftItemViewModel in Aircrafts.Where(x=>x.IsInTargetSelectionMode))
+            foreach (IAircraftItemViewModel aircraftItemViewModel in Aircrafts.Where(x => x.IsInTargetSelectionMode))
             {
                 aircraftItemViewModel.AddToPlannedRoute(target);
             }
@@ -171,24 +177,31 @@ namespace XPlaneLauncher.ViewModels
 
         public bool ReceiveWeakEvent(Type managerType, object sender, EventArgs e)
         {
-            if (sender is IAircraftItemViewModel aircraft && e is PropertyChangedEventArgs args && !_changingSelectedAicraftFromList)
+            if (sender is IAircraftItemViewModel aircraft && e is PropertyChangedEventArgs args &&
+                !_changingSelectedAicraftFromList)
             {
                 if (args.PropertyName == nameof(aircraft.IsSelected))
                 {
                     _changingSelectedAicraftFromMap = true;
                     SelectedAircraft = aircraft;
-                    _changingSelectedAicraftFromMap = false; 
-                }else if (args.PropertyName == nameof(aircraft.IsInTargetSelectionMode)) {
-                    if (aircraft.IsInTargetSelectionMode) {
+                    _changingSelectedAicraftFromMap = false;
+                }
+                else if (args.PropertyName == nameof(aircraft.IsInTargetSelectionMode))
+                {
+                    if (aircraft.IsInTargetSelectionMode)
+                    {
                         IsMapInSelectionMode = true;
                     }
-                    else {
+                    else
+                    {
                         IsMapInSelectionMode = false;
                     }
-                }else if (args.PropertyName == nameof(aircraft.PathToTarget))
+                }
+                else if (args.PropertyName == nameof(aircraft.PathToTarget))
                 {
                     UpdateTargetPaths();
-                }else if (args.PropertyName == nameof(aircraft.PlannedRoutePoints))
+                }
+                else if (args.PropertyName == nameof(aircraft.PlannedRoutePoints))
                 {
                     UpdatePathsPoints();
                 }
@@ -212,7 +225,7 @@ namespace XPlaneLauncher.ViewModels
         private void UpdateTargetPaths()
         {
             PathsToTarget.Clear();
-            foreach (IAircraftItemViewModel aircraftItemViewModel in Aircrafts.Where(x=>x.PathToTarget.Any()))
+            foreach (IAircraftItemViewModel aircraftItemViewModel in Aircrafts.Where(x => x.PathToTarget.Any()))
             {
                 PathsToTarget.Add(aircraftItemViewModel.PathToTarget.First());
             }
