12c9bbe5f4d0cbd67b59fe27190712d3b64e6c9f
Bernd Boruttau <b.boruttau@queo-group.com>
2020-02-15 08:44:58 +0100

    work in prgress - #50 - Removed GeoJson
    and added csv helper

--- XPlaneLauncher/Persistence/Impl/LogbookEntryTrackDao.cs
+++ XPlaneLauncher/Persistence/Impl/LogbookEntryTrackDao.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using GeoJSON.Net.Feature;
-using GeoJSON.Net.Geometry;
 using MapControl;
-using Newtonsoft.Json;
 
 namespace XPlaneLauncher.Persistence.Impl {
     public class LogbookEntryTrackDao : ILogbookEntryTrackDao {
@@ -15,27 +11,11 @@ namespace XPlaneLauncher.Persistence.Impl {
         }
 
         public IList<Location> GetTrack(FileInfo trackFile) {
-            string trackFileContent = File.ReadAllText(trackFile.FullName);
-            FeatureCollection points = JsonConvert.DeserializeObject<FeatureCollection>(trackFileContent);
-            IList<Location> track = new List<Location>();
-            foreach (Point point in points.Features.Select(x => x.Geometry as Point)) {
-                track.Add(new Location(point.Coordinates.Latitude, point.Coordinates.Longitude));
-            }
-
-            return track;
+            return new List<Location>();
         }
 
         public void Save(FileInfo trackFile, IList<Location> track) {
-            /* Using Feature and FeatureCollection to be able to ad additional information to each location like altitude, heading, airspeed, ... */
-            FeatureCollection features = new FeatureCollection();
-            foreach (Location location in track) {
-                Position position = new Position(location.Latitude, location.Longitude);
-                Point point = new Point(position);
-                features.Features.Add(new Feature(point));
-            }
-
-            string geoJson = JsonConvert.SerializeObject(features);
-            File.WriteAllText(trackFile.FullName, geoJson);
+            
         }
     }
 }
