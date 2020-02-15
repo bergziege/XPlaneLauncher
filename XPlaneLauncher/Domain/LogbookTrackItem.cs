bb62faf6c3b16f4cb25adf14c01ad1640dced779
Bernd Boruttau <b.boruttau@queo-group.com>
2020-02-15 15:11:36 +0100

    work in prgress - #50 - csv styling

--- XPlaneLauncher/Domain/LogbookTrackItem.cs
+++ XPlaneLauncher/Domain/LogbookTrackItem.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using CsvHelper.Configuration.Attributes;
 
 namespace XPlaneLauncher.Domain {
     public class LogbookTrackItem {
-        /// <summary>
-        ///     Initialisiert eine neue Instanz der <see cref="T:System.Object" />-Klasse.
-        /// </summary>
-        public LogbookTrackItem(DateTime timestamp) {
-            Timestamp = timestamp;
-        }
-
+        [Index(3)]
+        [Name("alt")]
         public double Alt { get; set; }
+
+        [Index(4)]
+        [Name("hdg")]
         public double Hdg { get; set; }
+
+        [Index(5)]
+        [Name("ias")]
         public double Ias { get; set; }
+
+        [Index(1)]
+        [Name("lat")]
         public double Lat { get; set; }
+
+        [Index(2)]
+        [Name("lon")]
         public double Lon { get; set; }
-        public DateTime Timestamp { get; }
+
+        [Index(0)]
+        [Name("time")]
+        public DateTime Time { get; set; }
     }
 }
