a1d10efdb5d99b07a35310c2e0d6d93082fcf889
Bernd Boruttau <b.boruttau@queo-group.com>
2020-02-15 08:37:15 +0100

    work in prgress - #50 - Added new track item class

---
+++ XPlaneLauncher/Domain/LogbookTrackItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XPlaneLauncher.Domain {
+    public class LogbookTrackItem {
+        /// <summary>
+        ///     Initialisiert eine neue Instanz der <see cref="T:System.Object" />-Klasse.
+        /// </summary>
+        public LogbookTrackItem(DateTime timestamp) {
+            Timestamp = timestamp;
+        }
+
+        public double? Alt { get; set; }
+        public double? Hdg { get; set; }
+        public double? Ias { get; set; }
+        public double? Lat { get; set; }
+        public double? Lon { get; set; }
+        public DateTime Timestamp { get; }
+    }
+}
